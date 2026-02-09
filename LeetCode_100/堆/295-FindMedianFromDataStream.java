6830028bfac7f059eaf120fb2db02fd960744710
jyl-0714-ly <2011441567@qq.com>
2026-02-09 23:14:41 +0800

    295-FindMedianFromDataStream

---
+++ LeetCode_100/堆/295-FindMedianFromDataStream.java
@@ -0,0 +1,42 @@
+class MedianFinder {
+    /*
+    题目：295.数据流的中位数        
+    中位数是有序整数列表中的中间值。如果列表的大小是偶数，则没有中间值，中位数是两个中间值的平均值。
+
+    例如 arr = [2,3,4] 的中位数是 3 。
+    例如 arr = [2,3] 的中位数是 (2 + 3) / 2 = 2.5 。
+    实现 MedianFinder 类:
+
+    MedianFinder() 初始化 MedianFinder 对象。
+
+    void addNum(int num) 将数据流中的整数 num 添加到数据结构中。
+
+    double findMedian() 返回到目前为止所有元素的中位数。与实际答案相差 10-5 以内的答案将被接受。
+    */
+    private final PriorityQueue<Integer> left = new PriorityQueue<>((a, b) -> b - a); // 最大堆
+    private final PriorityQueue<Integer> right = new PriorityQueue<>(); // 最小堆
+
+    public void addNum(int num) {
+        if (left.size() == right.size()) {
+            right.offer(num);
+            left.offer(right.poll());
+        } else {
+            left.offer(num);
+            right.offer(left.poll());
+        }
+    }
+
+    public double findMedian() {
+        if (left.size() > right.size()) {
+            return left.peek();
+        }
+        return (left.peek() + right.peek()) / 2.0;
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
