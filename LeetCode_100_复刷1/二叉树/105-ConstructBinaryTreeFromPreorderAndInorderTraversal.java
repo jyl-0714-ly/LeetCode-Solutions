7a37903da7acc5bbde4ab01be75c5420d49b2f67
jyl-0714-ly <2011441567@qq.com>
2026-06-02 20:50:27 +0800

    复刷105

---
+++ LeetCode_100_复刷1/二叉树/105-ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int[] preorder;
+    HashMap<Integer, Integer> dic = new HashMap<>();
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        this.preorder = preorder;
+        for(int i = 0; i<inorder.length; i++){
+            dic.put(inorder[i], i);
+        }
+        return recur(0,0,inorder.length-1);
+    }
+    public TreeNode recur(int root, int left, int right){
+        if(left > right){
+            return null;
+        }
+        TreeNode node = new TreeNode(preorder[root]);
+        int i = dic.get(preorder[root]);
+        node.left = recur(root + 1, left, i - 1);
+        node.right = recur(root + i - left + 1, i + 1, right);
+        return node;
+    }
+}
