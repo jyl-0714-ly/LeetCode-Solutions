d67dfaafd91c3b66d2e780dbd9650a941116ea6f
jyl-0714-ly <2011441567@qq.com>
2026-06-09 17:45:17 +0800

    复刷34

---
+++ LeetCode_100_复刷1/二分查找/34-FindFirstAndLastPositionOfElementInSortedArrayList.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int start = lowerBound(nums, target);
+        if(start == nums.length || nums[start] != target){
+            return new int[]{-1,-1};
+        }
+        int end = lowerBound(nums, target+1)-1;
+        return new int[]{start,end};
+    }
+    private int lowerBound(int[] nums, int target){
+        int left = 0;
+        int right = nums.length-1;
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(nums[mid] >= target){
+                right = mid - 1;
+            }else{
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
