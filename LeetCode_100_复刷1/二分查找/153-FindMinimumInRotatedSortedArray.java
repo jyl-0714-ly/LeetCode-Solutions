3c36186acc7b6b58ed7bf57c7d2061e82b08f8a8
jyl-0714-ly <2011441567@qq.com>
2026-06-11 14:52:31 +0800

    复刷153

---
+++ LeetCode_100_复刷1/二分查找/153-FindMinimumInRotatedSortedArray.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int len = nums.length;
+        int left = 0;
+        int right = nums.length - 1;
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(nums[mid] > nums[len - 1]){
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+        return nums[left];
+    }
+}
