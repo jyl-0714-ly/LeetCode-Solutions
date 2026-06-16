bdf075be47a617ae43fb94003495070f8060367c
jyl-0714-ly <2011441567@qq.com>
2026-06-16 18:05:33 +0800

    复刷283

---
+++ LeetCode_100_复刷1/双指针/283-MoveZeroes.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        for(int i = 0, j = 1; j < nums.length; j++){
+            if(nums[i] == 0 && nums[j] != 0){
+                nums[i] = nums[j];
+                nums[j] = 0;
+                i++;
+            }else if(nums[i] != 0){
+                i++;
+            }
+        }
+    }
+}
